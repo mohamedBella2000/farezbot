e6b0c22c6a74392d26db20d286d1d9e58d68f3dc
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-05-01 22:35:14 +0700

    Update client.js

--- client.js
+++ client.js
@@ -38,7 +38,7 @@ const {
 } = require('./system/neoxrApi')
 
 // Get free apikey register at https://api.neoxr.eu.org
-global.Api = new NeoxrApi('N0xSU8Yvu6')
+global.Api = new NeoxrApi('IVQO4ntU')
 
 const start = async () => {
    global.client = Socket({
