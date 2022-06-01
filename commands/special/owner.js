b75a8a3b0b1635cbb467dc14a9e57b760168782f
Faarrzz <91479112+Faarrzz@users.noreply.github.com>
2022-06-01 10:28:35 +0700

    Update owner.js
    Bzz

--- commands/special/owner.js
+++ commands/special/owner.js
@@ -3,7 +3,7 @@ exports.run = {
    async: async (m, {
       client
    }) => {
-      client.6281575900615(m.chat, `Farz`, m.sender.split('@')[0], m)
+      client.6281575900615(m.chat, 'Farz', m.sender.split('@')[0], m)
    },
    error: false,
    cache: true,
