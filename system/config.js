53fef95a298d14490a240b92ff9cf550ae093cd6
Faaarzzz <90979977+Faaarzzz@users.noreply.github.com>
2022-07-16 19:27:18 +0700

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,26 @@
+global.owner = ['6285692949920','6281575900615','447441414065']
+global.max_upload = 200 // 200MB (1 = 1MB)
+
+global.status = {
+   wait: Func.texted('bold', 'Processed . . .'),
+   invalid: Func.texted('bold', 'URL is Invalid!'),
+   wrong: Func.texted('bold', 'Wrong format!'),
+   getdata: Func.texted('bold', 'Scraping metadata . . .'),
+   fail: Func.texted('bold', 'Can\'t get metadata!'),
+   error: Func.texted('bold', 'Error occurred!'),
+   errorF: Func.texted('bold', 'Sorry this feature is in error.'),
+   premium: Func.texted('bold', 'This feature only for premium user.'),
+   owner: Func.texted('bold', 'This command only for owner.'),
+   god: Func.texted('bold', 'This command only for Master'),
+   group: Func.texted('bold', 'This command will only work in groups.'),
+   botAdmin: Func.texted('bold', 'This command will work when I become an admin.'),
+   admin: Func.texted('bold', 'This command only for group admin.'),
+   private: Func.texted('bold', 'Use this command in private chat.')
+}
+
+global.users = global.db.users
+global.groups = global.db.groups
+global.chats = global.db.chats
+global.sticker = global.db.sticker
+global.statistic = global.db.statistic
+global.setting = global.db.setting
